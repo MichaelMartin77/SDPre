071299d7c97177b53a5f67a8ed99cf5b86658d88
Michael <martin.michael1477@gmail.com>
2024-07-30 22:35:22 -0500

    objective2 and 3 commit

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Michael Martin";
+    int age = 23;
+    String state = "Texas";
+
+   System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+    
+  }
+}
---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,13 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+
+    int year = 2000; 
+    String month = "Novemember"; 
+    boolean isWeekend = false; 
+
+    System.out.println(year);
+    System.out.println(month);
+    System.out.println(isWeekend);
+
+  }
+}
